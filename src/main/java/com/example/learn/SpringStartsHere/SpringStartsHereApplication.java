8b31c0518c53d26045befa253c27b6172b2a40e4
Rewansh <rewansh57@gmail.com>
2025-05-05 14:30:58 +0530

    These are the project folders

---
+++ src/main/java/com/example/learn/SpringStartsHere/SpringStartsHereApplication.java
@@ -0,0 +1,30 @@
+package com.example.learn.SpringStartsHere;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStartsHereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStartsHereApplication.class,args);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}}
---
+++ src/test/java/com/example/learn/SpringStartsHere/SpringStartsHereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learn.SpringStartsHere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStartsHereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
